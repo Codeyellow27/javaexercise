85495f00b6a35664af34bcde4b1643500516b26e
Kevin Lio <45798351+Codeyellow27@users.noreply.github.com>
2019-05-20 01:39:50 +0800

    myfirstpythonwork

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+message = "hello python world"
+print(message)
+
+message = "hello Kevin."
+print(message)
+
+name = "liu kevin"
+print(name.title())
+
+mingzi = "KKK lllKKll"
+print(mingzi.title())
+print(mingzi.lower())
+print(mingzi.upper())
+
+first_name = "liu"
+last_name = "kevin"
+name = first_name.title()+" "+last_name.title()
+print(name)
+
+print("\tpython\tnihao\n\thahaha")
+print("Language:\n\tJava\n\tPython\n\tJavaScript")
+
+favorite_language = ' Python '
+print(favorite_language)
+favorite_language.lstrip()
+print(favorite_language)
